2816f277154f6033bc5eb7ce9679d3a73097842c
Trevor Stone <github@trevorstone.org>
2018-12-29 16:34:53 -0700

    Authenticate handle-checker route

--- app/routes/handle-checker.js
+++ app/routes/handle-checker.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import _ from 'lodash';
 
-export default class HandleCheckerRoute extends Route {
+export default class HandleCheckerRoute extends Route.extend(AuthenticatedRouteMixin) {
   model() {
     return this.ajax.request('handles')
         .then((result) => _.sortBy(result.data, [(h) => h.name.toLowerCase(), 'entityType']));
