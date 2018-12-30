f742eed4e9a703a3f036e99c30c4c644b1cf7fa1
Trevor Stone <github@trevorstone.org>
2018-12-30 00:24:41 -0700

    Fix alert-group test

--- tests/integration/components/alert-group-test.js
+++ tests/integration/components/alert-group-test.js
@@ -6,21 +6,29 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | alert-group', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders with empty group', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('heading', 'Test Heading');
+    this.set('description', 'Test Description');
+    this.set('group', []);
 
-    await render(hbs`{{alert-group}}`);
+    await render(hbs`{{alert-group group=group heading=heading description=description}}`);
 
-    assert.dom(this.element).hasText('');
+    assert.dom(this.element).hasText('Test Heading\nTest Description');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      {{#alert-group}}
-        template block text
-      {{/alert-group}}
-    `);
+  test('it renders with one alert', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('heading', 'Test Heading');
+    this.set('description', 'Test Description');
+    this.set('group', [{title: 'Alert Title', description: 'Alert Description', no_opt_out: true}]);
 
-    assert.dom(this.element).hasText('template block text');
+    await render(hbs`{{alert-group group=group heading=heading description=description}}`);
+
+    assert.dom('tr:first-of-type').hasText('Test Heading\nTest Description');
+    assert.dom('.alert-pref-desc').hasText('Alert Title\nAlert Description');
+    assert.dom('.alert-pref-address').hasText('Cannot be opted out of');
   });
 });
