e0aa280ccd5f2d7aef92a3c8fefbae2a53b80297
Mike Burg <mike@burg.me>
2019-01-13 09:53:45 -0800

    Person page layout tweaks.

--- app/components/person-photo.js
+++ app/components/person-photo.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 import { argument } from '@ember-decorators/argument';
+import { classNames } from '@ember-decorators/component';
 
+@classNames('mugshot', 'float-sm-right')
 export default class PersonPhotoComponent extends Component {
   @argument photo;
 }
