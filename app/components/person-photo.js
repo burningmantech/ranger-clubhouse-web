37f40bba44627934d31e79e6b8d539c6cdebc111
Mike Burg <mike@burg.me>
2019-01-13 17:16:54 -0800

    Browsers fixes and page tweaks.
    - Converted from ranger-logo and spinner svg to png & gif respectively to support IE Edge & Explorer.
    - Reset Password page renders correctly under IE.
    - Fixed photo & form page layout with Safari & IE, and form label alignment with Firefox. Photo is "groupped" with the first few form rows instead of the entire form.
    - Small refactor to move the ranger logo from all the different sidebar components into ch-sidebar.
    - Moved the "are you interested in.." boxes to the right side on the login screen.
    - Moved various images from assets into assets/images

--- app/components/person-photo.js
+++ app/components/person-photo.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 import { argument } from '@ember-decorators/argument';
 import { classNames } from '@ember-decorators/component';
 
-@classNames('mugshot', 'float-sm-right')
+@classNames('mugshot', 'float-md-right')
 export default class PersonPhotoComponent extends Component {
   @argument photo;
 }
