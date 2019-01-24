d9b6c7abe65b069441d79e667113cf630c5d445d
Mike Burg <mike@burg.me>
2019-01-23 21:58:38 -0800

    work on alert-group integration test. setting other test to skip for now.

--- tests/integration/components/alert-group-test.js
+++ tests/integration/components/alert-group-test.js
@@ -6,29 +6,86 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | alert-group', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders with empty group', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-    this.set('heading', 'Test Heading');
-    this.set('description', 'Test Description');
-    this.set('group', []);
+  function setupAlertTest(props) {
+    props.set('heading', 'Test Heading');
+    props.set('description', 'Test Description');
+    props.set('email', 'ranger@example.com');
+    props.set('numbers', { off_playa: { phone: '444-444-4444' }, on_playa: { phone: '555-555-555' } });
+  }
 
-    await render(hbs`{{alert-group group=group heading=heading description=description}}`);
+  test('it renders off playa only', async function(assert) {
+    setupAlertTest(this);
 
-    assert.dom(this.element).hasText('Test Heading\nTest Description');
+    this.set('group', [
+      { title: 'Title1', description: 'Description1', on_playa: true }
+    ]);
+
+
+    await render(hbs`{{alert-group group=group email=email numbers=numbers heading=heading description=description}}`);
+
+    const row = assert.dom('div.row');
+    row.exists();
+    row.hasText(/Title1/);
+    row.hasText(/Description1/);
+    row.hasText(/555-555-555/);
+    row.doesNotIncludeText('444-444-4444');
   });
 
-  test('it renders with one alert', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-    this.set('heading', 'Test Heading');
-    this.set('description', 'Test Description');
-    this.set('group', [{title: 'Alert Title', description: 'Alert Description', no_opt_out: true}]);
+  test('it renders on playa only', async function(assert) {
+    setupAlertTest(this);
+    this.set('group', [
+      { title: 'Title1', description: 'Description1', on_playa: false }
+    ]);
 
-    await render(hbs`{{alert-group group=group heading=heading description=description}}`);
+    await render(hbs`{{alert-group group=group email=email numbers=numbers heading=heading description=description}}`);
 
-    assert.dom('tr:first-of-type').hasText('Test Heading\nTest Description');
-    assert.dom('.alert-pref-desc').hasText('Alert Title\nAlert Description');
-    assert.dom('.alert-pref-address').hasText('Cannot be opted out of');
+    const row = assert.dom('div.row');
+    row.exists();
+    row.hasText(/444-444-4444/);
+    row.doesNotIncludeText('555-555-5555');
+  });
+
+  test('it renders not optout', async function(assert) {
+    setupAlertTest(this);
+    this.set('group', [
+      { title: 'Title1', description: 'Description1', on_playa: true, no_opt_out: true }
+    ]);
+
+    await render(hbs`{{alert-group group=group email=email numbers=numbers heading=heading description=description}}`);
+
+    const row = assert.dom('div.row');
+    row.exists();
+    row.includesText('SMS cannot be opted out of');
+    row.includesText('Email cannot be opted out of');
+    row.doesNotIncludeText('444-444-4444');
+    row.doesNotIncludeText('555-555-5555');
+  });
+
+  test('it renders email only', async function(assert) {
+    setupAlertTest(this);
+    this.set('group', [
+      { title: 'Title1', description: 'Description1', email_only: true }
+    ]);
+
+    await render(hbs`{{alert-group group=group email=email numbers=numbers heading=heading description=description}}`);
+
+    const row = assert.dom('div.row');
+    row.exists();
+    row.includesText('ranger@example.com');
+    row.includesText('only email used');
+  });
+
+  test('it renders sms only', async function(assert) {
+    setupAlertTest(this);
+    this.set('group', [
+      { title: 'Title1', description: 'Description1', sms_only: true }
+    ]);
+
+    await render(hbs`{{alert-group group=group email=email numbers=numbers heading=heading description=description}}`);
+
+    const row = assert.dom('div.row');
+    row.exists();
+    row.doesNotIncludeText('ranger@example.com');
+    row.includesText('only SMS used');
   });
 });
