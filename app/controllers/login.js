93dbafc2e7b0a01d27f8e10b749c829a52c9f40f
Mike Burg <mike@burg.me>
2019-01-27 21:31:11 -0800

    Restructured the login process.
    - Login to C2, then C1. Allow the user to continue into C2 even if C1 fails.
    - The backend returns a status code instead of message.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -6,28 +6,61 @@ import { action } from '@ember-decorators/object';
 export default class LoginController extends Controller {
   loginValidations = LoginValidations;
   isSubmitting = false;
+  loginError = null;
 
   apiLogin(credentials) {
     this.set('isSubmitting', true);
     return this.session.authenticate('authenticator:jwt', credentials)
-          .catch((response) => {
-            if (response.status == 401) {
-              const data = response.json ? response.json : response.payload;
-              this.toast.error(data.error);
-            } else {
-              this.house.handleErrorResponse(response)
-            }
-          }).finally(() => this.set('isSubmitting', false));
+      .then((result) => {
+        if (ENV['dualClubhouse']) {
+          this.classicLogin(credentials);
+        }
+      })
+      .catch((response) => {
+        if (response.status == 401) {
+          const data = response.json ? response.json : response.payload;
+          this.set('loginError', data.status);
+        } else {
+          this.house.handleErrorResponse(response)
+        }
+      }).finally(() => this.set('isSubmitting', false));
+  }
+
+  async classicLogin(credentials) {
+    //
+    // Attempt to authorize with Classic Clubhouse before trying for
+    // Clubhouse 2.0
+    //
+
+    await this.ajax.post('/?DMSc=security&DMSm=login&json=1', {
+        headers: {
+          // Avoid CORS pre-flight
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        },
+        host: ENV['clubhouseClassicUrl'],
+        data: {
+          email: credentials.identification,
+          password: credentials.password
+        },
+      }).then((response) => {
+        // So far, so good. set the clubhouse classic PHP session cookie,
+        // and then try for Clubhouse 2.0 authentication.
+        const session = response.session;
+        this.setCookie(session.name, session.id);
+      })
+      .catch((response) => {
+        this.toast.error('There was a problem logging into the Classic Clubhouse. Some features may not be available.');
+      });
   }
 
   setCookie(name, value, days) {
     let expires = "";
     if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
   }
 
   @action
@@ -35,6 +68,7 @@ export default class LoginController extends Controller {
     if (!isValid)
       return;
 
+    this.set('loginError', null);
     this.toast.clear();
     let credentials = model.getProperties('identification', 'password');
 
@@ -44,34 +78,6 @@ export default class LoginController extends Controller {
       height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
     };
 
-    if (!ENV['dualClubhouse']) {
-      // Not running in Dual Clubhouse mode ..
-      return this.apiLogin(credentials);
-    }
-
-    //
-    // Attempt to authorize with Classic Clubhouse before trying for
-    // Clubhouse 2.0
-    //
-
-    return this.ajax.post('/?DMSc=security&DMSm=login&json=1',
-      {
-        headers: {
-          // Avoid CORS pre-flight
-          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        },
-        host: ENV['clubhouseClassicUrl'],
-        data: { email: credentials.identification, password: credentials.password },
-      }
-    ).then((response) => {
-      // So far, so good. set the clubhouse classic PHP session cookie,
-      // and then try for Clubhouse 2.0 authentication.
-      const session = response.session;
-      this.setCookie(session.name, session.id);
-      return this.apiLogin(credentials);
-    })
-    .catch((response) => {
-        this.house.handleErrorResponse(response);
-    }).finally(() => this.set('isSubmitting', false));
+    this.apiLogin(credentials);
   }
 }
