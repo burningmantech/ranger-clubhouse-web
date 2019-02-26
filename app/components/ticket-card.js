b9366c26a58f93e01dc5c57f2bfd2cffd0d1cad5
Mike Burg <mike@burg.me>
2019-02-25 22:25:00 -0800

    Killed fancy-schmancy modern design ticket boxes on announcement page -- going with basic text instead.

--- app/components/ticket-card.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-import { argument } from '@ember-decorators/argument';
-import { tagName } from '@ember-decorators/component';
-
-@tagName('')
-export default class TicketCardComponent extends Component {
-  @argument('object') ticket;
-}
