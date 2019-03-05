a80657f38143d07b6e13e0ccdfeebe2d1f27274f
Mike Burg <mike@burg.me>
2019-03-05 15:42:52 -0800

    UI Tweaks and elimination of most sidebar components.
    - When the narbav is collapsed, hide the callsign dropdown menu, and
      show the callsign as a header with a right floated logout button when navbar.
      (Better layout for smartphones.)
    
    - Training page tweaks for smartphones.
    
    - Fixed up search bar form so there's no "gutter spill" on the right.
    
    - Timesheet correction UI changes - using green forward/back links with arrows.
      (mirrors the new Ticketing UI.)
    
    - Removed sidebar components and folded the html into the root template.
      The sidebars were only used in one place, doesn't make sense to split
      the files up. The login sidebar component is the only one left standing.

--- app/components/person-sidebar.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-import { argument } from '@ember-decorators/argument';
-import { tagName } from '@ember-decorators/component';
-
-@tagName('')
-export default class PersonSidebarComponent extends Component {
-  @argument('object') person;
-}
