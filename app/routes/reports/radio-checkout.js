b781ae4e0702994920170341bd1645501dd8231a
Mike Burg <mike@burg.me>
2019-03-05 17:54:39 -0800

    ggrrr.. must. lint. first. before. check. in. yo.

--- app/routes/reports/radio-checkout.js
+++ app/routes/reports/radio-checkout.js
@@ -49,10 +49,4 @@ export default class ReportsRadioCheckoutRoute extends Route {
   _isSet(value) {
     return (parseInt(value) || value == 'true');
   }
-
-  resetController(controller, isExiting) {
-    if (isExiting) {
-      controller.set('year', null);
-    }
-  }
 }
