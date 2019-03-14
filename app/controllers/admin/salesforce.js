3a8658e46f00e39bb23a7f13665dfb78b5dccaa9
Mike Burg <mike@burg.me>
2019-03-13 17:54:17 -0700

    check point.

--- app/controllers/admin/salesforce.js
+++ app/controllers/admin/salesforce.js
@@ -3,16 +3,51 @@ import { set } from '@ember/object';
 import { action } from '@ember-decorators/object';
 
 export default class AdminSalesforceController extends Controller {
+  commit = false;
+  updateSalesforce = false;
+  nonTestAccounts = false;
+  showAll = false;
+  resetTestAccounts = false;
+
   @action
   import() {
     this.toast.clear();
     this.set('isSubmitting', true);
-    this.ajax.request('salesforce/import').then((result) => {
+    const options = {};
+    if (this.showAll) {
+      options.showall = 1;
+    }
+
+    if (this.updateSalesforce) {
+      options.update_sf = 1;
+    }
+
+    if (this.nonTestAccounts) {
+      options.non_test_accounts = 1;
+    }
+
+    if (this.resetTestAccounts) {
+      options.reset_test_accounts = 1;
+    }
+
+    if (this.commit) {
+      options.commit = 1;
+    }
+
+    this.ajax.request('salesforce/import', { data: options }).then((result) => {
       this.set('importStatus', result.status);
       this.set('importMessage', result.message);
       this.set('accounts', result.accounts);
-    }).catch((response) => this.house.handleErrorResponse(response))
-    .finally(() => this.set('isSubmitting', false) );
+    })
+    .catch((response) => this.house.handleErrorResponse(response))
+    .finally(() => {
+      this.set('isSubmitting', false);
+      this.set('commit', false);
+      this.set('updateSalesforce', false);
+      this.set('nonTestAccounts', false);
+      this.set('showAll', false);
+      this.set('resetTestAccounts', false);
+    });
   }
 
   @action
@@ -23,5 +58,10 @@ export default class AdminSalesforceController extends Controller {
   @action
   toggleAccount(account) {
     set(account, 'showing', !account.showing);
+  }
+
+  @action
+  toggleHelp() {
+    this.set('showHelp', !this.showHelp);
   }
 }
--- app/routes/admin/salesforce.js
+++ app/routes/admin/salesforce.js
@@ -15,5 +15,11 @@ export default class AdminSalesforceRoute extends Route {
     controller.set('importStatus', '');
     controller.set('importMessage', '');
     controller.set('accounts', []);
+
+    controller.set('commit', false);
+    controller.set('updateSalesforce', false);
+    controller.set('nonTestAccounts', false);
+    controller.set('showAll', false);
+    controller.set('resetTestAccounts', false);
   }
 }
