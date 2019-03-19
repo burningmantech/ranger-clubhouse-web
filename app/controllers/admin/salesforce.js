8ca8c3e4f5db6c49151a20e692c576b9bdc443d4
Mike Burg <mike@burg.me>
2019-03-19 13:35:57 -0700

    Group SF import table by status. Fixed person link.

--- app/controllers/admin/salesforce.js
+++ app/controllers/admin/salesforce.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { set } from '@ember/object';
-import { action } from '@ember-decorators/object';
+import { action, computed } from '@ember-decorators/object';
+import groupBy from 'clubhouse/utils/group-by';
 
 export default class AdminSalesforceController extends Controller {
   commit = false;
@@ -9,13 +10,54 @@ export default class AdminSalesforceController extends Controller {
   showAll = false;
   resetTestAccounts = false;
 
+  statusLabels = {
+    'succeeded': 'Successfully imported into Clubhouse',
+    'invalid': 'Invalid record status',
+    'imported': 'Mark by Salesforce as already imported into Clubhouse',
+    'ready': 'Ready for import',
+    'notready': 'Not ready for import',
+    'already_exists_bpguid': 'BPGUID Already Exists',
+    'already_exists_email': 'Email Already Exists',
+    'already_exists_callsign': 'Callsign Already Exists',
+    'reset': 'Reset Salesforce status',
+  };
+
   resetFlags() {
-    this.set('expandedAll', false);
     this.set('createAccounts', false);
     this.set('updateSalesforce', false);
     this.set('nonTestAccounts', false);
     this.set('showAll', false);
     this.set('resetTestAccounts', false);
+  }
+
+  @computed('accounts')
+  get accountGroups() {
+    const groups = groupBy(this.accounts, 'status');
+
+    const ready = groups.find((g) => g.status == 'ready');
+    const imported = groups.find((g) => g.status == 'imported');
+    const succeeded = groups.find((g) => g.status == 'succeeded');
+
+    // Order the groups by ready, imported, and succeeded
+    if (succeeded) {
+      groups.removeObject(succeeded);
+      groups.unshift(succeeded);
+    }
+    if (imported) {
+      groups.removeObject(imported);
+      groups.unshift(imported);
+    }
+    if (ready) {
+      groups.removeObject(ready);
+      groups.unshift(ready);
+    }
+
+    groups.forEach((g) => {
+      g.statusLabel = this.statusLabels[g.status] || `Unknown status [${g.status}]`;
+      g.items = g.items.sortBy('callsign');
+    });
+
+    return groups;
   }
 
   @action
@@ -57,10 +99,10 @@ export default class AdminSalesforceController extends Controller {
   }
 
   @action
-  toggleAll() {
-    this.set('expandedAll', !this.expandedAll);
-    const show = this.expandedAll;
-    this.accounts.forEach((account) => set(account, 'showing', show));
+  toggleAll(group) {
+    set(group, 'expandedAll', !group.expandedAll);
+    const show = group.expandedAll;
+    group.items.forEach((account) => set(account, 'showing', show));
   }
 
   @action
