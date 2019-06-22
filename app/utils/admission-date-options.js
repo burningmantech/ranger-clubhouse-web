ba0dcb06e661975a29dc58609a8c792a1401e70d
Mike Burg <mike@burg.me>
2019-06-22 08:14:29 -0700

    Fixed wap date options, trim spaces on config setting as a precaution.

--- app/utils/admission-date-options.js
+++ app/utils/admission-date-options.js
@@ -7,8 +7,19 @@ export default function admissionDateOptions(year, wapDateRange) {
 
   let low = 5, high = 26;
   if (wapDateRange != null) {
-    [low, high] = wapDateRange.split('-');
+    [low, high] = wapDateRange.replace(/\s/,'').split('-');
+    low = parseInt(low);
+    high = parseInt(high);
+
+    if (!low) {
+      low = 5;
+    }
+
+    if (!high) {
+      high = 26;
+    }
   }
+
 
   for (let day = high; day >= low; day--) {
     const date = `${year}-08-${day < 10 ? '0'+day : day}`;
