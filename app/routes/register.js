55082f8d4888e68633ee277230aae79c0c308f82
Mike Burg <mike@burg.me>
2020-01-17 15:02:41 -0800

    Registration page gatekeeping.
    - Present a series of questions to ask why the person is registration
    - Direct the user to the approx. rangers.burningman.org page on how to submit an application
    - Confirm with the user that auditing is not a path to becoming a Ranger.
    - fixed forms bug.

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,5 +1,29 @@
 import Route from '@ember/routing/route';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
+import { isEmpty } from '@ember/utils';
 
 export default class RegisterRoute extends Route.extend(UnauthenticatedRouteMixin) {
+  queryParams = {
+    step: { refreshModel: true }
+  };
+
+  model({ step }){
+    if (isEmpty(step)) {
+      step = 'ask-intent';
+    }
+
+    return { step };
+  }
+
+  setupController(controller, model) {
+      super.setupController(...arguments);
+
+      controller.set('step', model.step);
+  }
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      controller.set('step', 'ask-intent');
+    }
+  }
 }
