6000d9158af106f23398247545cc1623222d5a7b
Mike Burg <mike@burg.me>
2020-02-05 12:58:18 -0800

    Updated ticketing off season message.

--- app/components/ticketing-offseason.js
+++ app/components/ticketing-offseason.js
@@ -1,14 +1,24 @@
-import Component from '@ember/component';
-
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import {computed} from '@ember/object';
 
 export default class TicketingOffseasonComponent extends Component {
-  ticketingInfo = null;
-  ticketPackage = null;
-  person = null;
-
-  @computed('ticketPackage.tickets')
+  @computed('args.ticketPackage.tickets')
   get bankedTickets() {
-    return this.ticketPackage.tickets.filter((ticket) => (ticket.status == 'banked' || ticket.status == 'qualified'));
+    return this.args.ticketPackage.tickets.filter((ticket) => (ticket.status == 'banked' || ticket.status == 'qualified'));
+  }
+
+  get eventYear() {
+    /*
+     * grrr. months are 0 to 11. seriously?
+     */
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    if (month >= 9) {
+      year++;
+    }
+
+    return year;
   }
 }
