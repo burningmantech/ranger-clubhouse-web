bd31e1ab432f4bb8b1c6304ad0ca13d697463846
Mike Burg <mike@burg.me>
2020-02-09 20:10:16 -0800

    Added MOTD filtering and converted motd template to Octane.

--- app/controllers/admin/motd.js
+++ app/controllers/admin/motd.js
@@ -24,10 +24,17 @@ export default class AdminMotdController extends Controller {
     }
     const isNew = this.entry.isNew;
 
+    if (!model.get('for_pnvs') && !model.get('for_auditors') && !model.get('for_rangers')) {
+      this.modal.info(null, 'Check one or more audiences to show the message to.');
+      return;
+    }
+
     model.save().then(() => {
       this.toast.success(`MOTD was successfully ${isNew ? 'created' : 'updated'}.`);
       this.motds.update();
       this.set('entry', null);
+    }).catch((response) => {
+      this.house.handleErrorResponse(response);
     });
   }
 
