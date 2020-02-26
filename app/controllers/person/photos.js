22ee13c9d141e9f51c2520a304c1fc9c3ced140f
Mike Burg <mike@burg.me>
2020-02-26 12:12:13 -0800

    removed unused @tracked.

--- app/controllers/person/photos.js
+++ app/controllers/person/photos.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
 export default class PersonPhotosController extends Controller {
   @action
