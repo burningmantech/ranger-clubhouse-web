9ae797ffcc3207f105c4b9b619b3fb41129ef768
Mike Burg <mike@burg.me>
2020-02-28 10:27:18 -0800

    Search All The Things! Asset searching added to the search bar.
    - Use '#barcode' or '#barcode:year' to search the current year or given year respectively.
    - Search asset page uses query params for linkability.
    - Search asset page results highlights the person who currently has the asset checked out.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,25 +1,16 @@
 import Controller from '@ember/controller';
-import { action, computed } from '@ember/object';
+import {action, computed} from '@ember/object';
 import EmberObject from '@ember/object';
-import { Role } from 'clubhouse/constants/roles';
-import { debounce, run } from '@ember/runloop';
-import { config } from 'clubhouse/utils/config';
-import { inject as service } from '@ember/service';
-import { inject as controller } from '@ember/controller';
+import {Role} from 'clubhouse/constants/roles';
+import {debounce, run} from '@ember/runloop';
+import {config} from 'clubhouse/utils/config';
+import {inject as service} from '@ember/service';
+import {inject as controller} from '@ember/controller';
 
 import moment from 'moment';
 
 import ENV from 'clubhouse/config/environment';
 import RSVP from 'rsvp';
-
-import $ from 'jquery';
-
-const ENTER = 13;
-const F1 = 112;
-/*const F2 = 113;
-const F3 = 114;
-const ESC = 27;
-*/
 
 // How often in milliseconds should a search be performed as
 // the user is typing.
@@ -40,20 +31,19 @@ export default class ApplicationController extends Controller {
 
   constructor() {
     super(...arguments);
-    /*
-     * Bind the keyup event on the body element to intercept the user's
-     * typing so ESC to as shortcuts to the search page.
-     */
 
-    $('body').bind('keyup', (event) => { // eslint-disable-line ember/jquery-ember-run
-      if (!(event.shiftKey && event.keyCode == F1)) {
+    /*
+     * Bind the keyup event so when Shift+F1 is pressed, the search bar is focused.
+     */
+    document.querySelector('body').addEventListener('keyup', (event) => {
+      if (!(event.shiftKey && event.key == "F1")) {
         return true;
       }
 
       event.preventDefault(); // eslint-disable-line ember/jquery-ember-run
 
       run.schedule('afterRender', () => {
-        $('#person-search-query input').focus();
+        document.querySelector('#search-bar input').focus();
       });
 
       return false;
@@ -77,7 +67,7 @@ export default class ApplicationController extends Controller {
               arr[i] = binStr.charCodeAt(i);
             }
 
-            callback(new Blob([arr], { type: type || 'image/png' }));
+            callback(new Blob([arr], {type: type || 'image/png'}));
 
           });
         }
@@ -116,17 +106,23 @@ export default class ApplicationController extends Controller {
   }
 
   isSubmitting = false;
-  showFKA = false;
 
   searchForm = {};
+
+  /**
+   * Save the search prefs when changed. The local store is used so the prefs persist in case the user
+   * reloads the page.
+   */
 
   @action
   searchFormChange() {
     this.house.setKey('person-search-prefs', this.searchForm);
   }
 
-  /*
-   * Called from route/application when the user is logged in.
+  /**
+   * Is the user allowed to see the search bar?
+   *
+   * @returns {boolean}
    */
 
   @computed('session.user.roles')
@@ -138,29 +134,39 @@ export default class ApplicationController extends Controller {
     }
   }
 
-  @computed('session.user.{is_on_duty_at_hq,has_hq_window}')
-  get modeOptions() {
-    const user = this.session.user;
-    const options = [{ id: 'account', title: 'Person Manage' }];
-
-    if (user.has_hq_window) {
-      options.push({ id: 'hq', title: 'HQ Window' });
-    }
-
-    if (user.hasRole([Role.ADMIN, Role.TIMESHEET_MANAGEMENT])) {
-      options.push({ id: 'timesheet', title: 'Timesheet Manage' });
-    }
-
-    return options;
-  }
-
   _modeRoutes = {
     'account': 'person.index',
     'hq': 'hq.index',
     'timesheet': 'person.timesheet'
   };
 
-  /*
+  /**
+   * Build up the options for search type
+   *
+   * account - redirect to Person Manage
+   * timesheet - redirect to the person timesheet management page
+   * hq - redirect to the HQ Window interface
+   *
+   * @returns {[{id: search-mode, title: search label}]}
+   */
+
+  @computed('session.user.{is_on_duty_at_hq,has_hq_window}')
+  get modeOptions() {
+    const user = this.session.user;
+    const options = [{id: 'account', title: 'Person Manage'}];
+
+    if (user.has_hq_window) {
+      options.push({id: 'hq', title: 'HQ Window'});
+    }
+
+    if (user.hasRole([Role.ADMIN, Role.TIMESHEET_MANAGEMENT])) {
+      options.push({id: 'timesheet', title: 'Timesheet Manage'});
+    }
+
+    return options;
+  }
+
+  /**
    * Called when the user changes the search mode.
    *
    * If the current route is a person or hq page, switch to the new view with displayed person.
@@ -168,6 +174,9 @@ export default class ApplicationController extends Controller {
    * HQ Window workers were viewing a person in the wrong mode, and wanting to use options to
    * change the view instead of using the 'Switch to {HQ Window,Person}' link -- because
    * playa brain is a thing and interfaces get used in ways you never intended.
+
+   * @param {string} fieldName
+   * @param {string} mode
    */
 
   @action
@@ -187,19 +196,24 @@ export default class ApplicationController extends Controller {
     this.transitionToRoute((this._modeRoutes[mode] || 'person.index'), person.id);
   }
 
-  /*
-   * Transition to the selected person. Clear the query and results, blur
-   * the input field
+  /**
+   * Transition to the selected person/asset. Clear the query and results, blur the input field.
+   *
+   * @param item
+   * @private
    */
 
-  _showPerson(person) {
-    const mode = this.searchForm.mode;
+  _showItem(item) {
+    if (this.searchType == 'asset') {
+      this.transitionToRoute('search.assets', {queryParams: {barcode: item.barcode, year: this.searchYear}});
+    } else {
+      this.transitionToRoute((this._modeRoutes[this.searchForm.mode] || 'person.index'), item.id);
+    }
 
     this.set('showSearchOptions', false);
     this.set('query', '');
     this.set('enterPressed', false);
-    this.transitionToRoute((this._modeRoutes[mode] || 'person.index'), person.id);
-    $('#person-search-query input').blur();
+    document.querySelector('#search-bar input').blur();
   }
 
   @action
@@ -213,9 +227,9 @@ export default class ApplicationController extends Controller {
    */
 
   @action
-  searchChangeAction(person) {
-    if (person) {
-      this._showPerson(person);
+  searchChangeAction(item) {
+    if (item) {
+      this._showItem(item);
     }
   }
 
@@ -236,9 +250,8 @@ export default class ApplicationController extends Controller {
    * Search for the person
    */
 
-  _performSearch([callsign, powerSelect], resolve, reject) {
-    const query = callsign.trim();
-    const form = this.searchForm;
+  _performSearch([query, powerSelect], resolve, reject) {
+    query = query.trim();
 
     // query has to be two characters or more..
     if (query.length < 2) {
@@ -246,7 +259,75 @@ export default class ApplicationController extends Controller {
     }
 
     // Person id lookup
-    this.set('searchById', query.startsWith('+'));
+    let type;
+    if (query.startsWith('#')) {
+      type = 'asset'
+    } else if (query.startsWith('+')) {
+      type = 'person-id';
+    } else {
+      type = 'person';
+    }
+
+    this.set('searchType', type);
+
+    if (type == 'asset') {
+      this._searchAsset(query, powerSelect, resolve, reject);
+    } else {
+      this._searchPerson(query, powerSelect, resolve, reject);
+    }
+  }
+
+  _searchAsset(query, powerSelect, resolve, reject) {
+    let year, barcode;
+
+    query = query.replace('#', '').replace(/ /g, '');
+    if (query.includes(':')) {
+      [barcode, year] = query.split(':');
+      if (year.length != 4) {
+        return resolve([]);
+      }
+    } else {
+      barcode = query;
+      year = this.house.currentYear();
+    }
+
+    this.set('searchYear', year);
+
+    this.ajax.request('asset', {data: {barcode, year}})
+      .then((results) => {
+        if (results.asset.length == 0) {
+          return resolve([]);
+        }
+
+        const asset = results.asset.firstObject;
+        const searchResults = [{
+          barcode: asset.barcode,
+          description: asset.description,
+          type: asset.temp_id,
+        }];
+
+        if (this.enterPressed) {
+          /*
+           * Allow EPS to render the list, and then choose the item.
+           * EPS will tend 'blur' correctly by removing the results box.
+           */
+          setTimeout(() => {
+            run.schedule('afterRender', () => {
+              powerSelect.actions.choose(asset);
+            });
+          }, 50);
+        }
+
+        this.set('searchResults', searchResults);
+        return resolve(searchResults);
+      }).catch((response) => {
+      this.house.handleErrorResponse(response);
+      return reject();
+    });
+  }
+
+  _searchPerson(query, powerSelect, resolve, reject) {
+    const form = this.searchForm;
 
     const params = {
       basic: 1,
@@ -309,7 +390,7 @@ export default class ApplicationController extends Controller {
       this.set('searchResults', people);
       return resolve(people);
     }).catch((response) => {
-      this.house.handleErrorResponse(response)
+      this.house.handleErrorResponse(response);
       return reject();
     });
   }
@@ -331,7 +412,7 @@ export default class ApplicationController extends Controller {
 
   @action
   searchKeydownAction(powerSelect, event) {
-    if (event.keyCode === ENTER) {
+    if (event.code === "Enter") {
       event.preventDefault();
 
       if (this.searchResults && this.searchResults.length == 1) {
@@ -351,6 +432,7 @@ export default class ApplicationController extends Controller {
     this.set('searchResults', null);
     this.set('showSearchOptions', (this.searchForm.mode != 'hq'));
     this.set('enterPressed', false);
+    this.set('searchYear', null);
   }
 
   @action
@@ -384,19 +466,18 @@ export default class ApplicationController extends Controller {
       }
     }
 
+    fields.push('#barcode');
+
     const arrayToSentence = (a, conjunction) => [a.slice(0, -1).join(', '), a.pop()].filter(w => w !== '').join(` ${conjunction} `);
 
     return `Enter a ${arrayToSentence(fields, 'or')} to search`;
   }
 
-  @computed('ENV')
   get applicationVersion() {
     return ENV.APP.version;
   }
 
-  @computed('ENV')
   get buildTimestamp() {
     return ENV.APP.buildTimestamp;
   }
-
 }
