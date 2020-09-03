e33cf77056c1869a8fed0dbeea496b9d69d37351
Victor Asteinza <civrot@users.noreply.github.com>
2020-09-02 17:58:49 -0700

    Add estimated hours to CSV export

--- app/controllers/reports/potential-shirts.js
+++ app/controllers/reports/potential-shirts.js
@@ -9,6 +9,7 @@ export default class ReportsPotentialShirtsController extends Controller {
     const CSV_COLUMNS = [
       { title: 'Callsign', key: 'callsign' },
       { title: 'Full Name', key: 'name' },
+      { title: `Estimated Hours in ${this.year}`, key: 'estimated_hours' },
       { title: `Hours in ${this.year}`, key: 'actual_hours' },
       { title: `T-Shirt Shirt (${this.threshold_ss} hrs)`, key: 'short_sleeve' },
       { title: `Long Sleeve Shirt (${this.threshold_ls} hrs)`, key: 'long_sleeve' },
@@ -18,6 +19,7 @@ export default class ReportsPotentialShirtsController extends Controller {
       return {
         callsign: row.callsign,
         name: `${row.first_name} ${row.middle_initial} ${row.last_name}`,
+        estimated_hours: row.estimated_hours,
         actual_hours: row.actual_hours,
         short_sleeve: row.earned_ss ? row.teeshirt_size_style : 'not earned',
         long_sleeve: row.earned_ls ? row.longsleeveshirt_size_style : 'not earned',
