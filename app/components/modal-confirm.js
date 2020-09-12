9cd692c5bc2fa68403594ce816a624e6a94c33a3
Mike Burg <mike@burg.me>
2020-09-12 10:42:00 -0700

    Re-enabling some linting rules.

--- app/components/modal-confirm.js
+++ app/components/modal-confirm.js
@@ -1,5 +1,8 @@
 import Component from '@glimmer/component';
+import { htmlSafe } from '@ember/string';
 
 export default class ModalConfirmComponent extends Component {
-
+  get htmlMessage() {
+    return htmlSafe(this.args.dialog.message);
+  }
 }
