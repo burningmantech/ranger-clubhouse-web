49dcb4ec053cba3c11ef291a62ab85da5d0132b9
Mike Burg <mike@burg.me>
2020-12-03 12:06:58 -0800

    More sidebar color/theme adjustments. Cleaned up & simplified sidebar CSS.

--- app/components/ch-sidebar.js
+++ app/components/ch-sidebar.js
@@ -1,8 +1,9 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { isEmpty} from '@ember/utils';
-import { inject as service } from '@ember/service';
-import $ from 'jquery';
+import {action} from '@ember/object';
+import {isEmpty} from '@ember/utils';
+import {inject as service} from '@ember/service';
+
+const COLLAPSED_PREF = 'sidebarCollapsed';
 
 export default class ChSidebarComponent extends Component {
   @service house;
@@ -13,39 +14,34 @@ export default class ChSidebarComponent extends Component {
 
   @action
   sidebarInserted() {
-    // Collapse/Expand icon
-    $('#collapse-icon').addClass('fa-angle-double-left');
-
     // Does the user want the sidebar collapsed?
-    if (this.house.getKey('sidebarCollapse')) {
+    if (this.house.getKey(COLLAPSED_PREF)) {
       this.sidebarToggle();
     }
   }
 
+  _toggleClass(selector, classNames) {
+    const classes = classNames.split(' ');
+    document.querySelectorAll(selector).forEach((element) => {
+      classes.forEach((name) => element.classList.toggle(name));
+    });
+  }
+
   @action
   sidebarToggle() {
-    let collapsed;
+    // Adjust the sidebar width and style
+    this._toggleClass('#sidebar-container', 'sidebar-expanded sidebar-collapsed');
 
-    $('.menu-collapsed').toggleClass('d-none');
-    $('.sidebar-submenu').toggleClass('d-none');
-    $('.submenu-icon').toggleClass('d-none');
-    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
-
-    // Treating d-flex/d-none on separators with title
-    var separatorTitle = $('.sidebar-separator-title');
-    if (separatorTitle.hasClass('d-flex')) {
-        separatorTitle.removeClass('d-flex');
-        collapsed = true;
-    } else {
-        separatorTitle.addClass('d-flex');
-        collapsed = false;
-    }
+    // Show or hide the group titles, link text, and ranger logo
+    this._toggleClass('.sidebar-group-title', 'd-none');
+    this._toggleClass('.sidebar-link-text', 'd-none')
+    this._toggleClass('.sidebar-ranger-logo', 'd-none');
 
     // Collapse/Expand icon
-    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+    this._toggleClass('#collapse-icon', 'fa-angle-double-left fa-angle-double-right');
 
     // Save the collapsed preference
-    this.house.setKey('sidebarCollapse', collapsed);
+    this.house.setKey(COLLAPSED_PREF, !this.house.getKey(COLLAPSED_PREF));
 
     return false;
   }
