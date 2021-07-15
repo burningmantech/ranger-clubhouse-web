8aef897e04e9665b517d1621b0a97c672e9bd979
mikeburg <mike@burg.me>
2021-07-14 23:21:38 -0700

    forgot a few files..

---
+++ app/routes/admin/bulk-positions.js
@@ -0,0 +1,27 @@
+import ClubhouseRoute from 'clubhouse/routes/clubhouse-route';
+import {ADMIN} from 'clubhouse/constants/roles';
+
+export default class AdminBulkPositionsRoute extends ClubhouseRoute {
+  roleRequired = ADMIN;
+
+  model() {
+    return this.ajax.request('position').then(({position}) => position);
+  }
+
+  setupController(controller, model) {
+    controller.setProperties({
+      positions: model,
+      committed: false,
+      granted: false,
+      people: null,
+      bulkForm: {callsigns: '', grant: false, positionId: null},
+      errorCount: 0,
+      successCount: 0
+    });
+
+    controller.positionsById = model.reduce((hash, p) => {
+      hash[p.id] = p;
+      return hash;
+    }, {});
+  }
+}
