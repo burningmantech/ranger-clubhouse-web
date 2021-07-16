894dff8a882e53ef23bf858ee2473281a2dba4de
mikeburg <mike@burg.me>
2021-07-15 18:06:16 -0700

    Various tweaks and bug fixes.
    - Fixed CSS so only the sidebar link text is underlined on hover. The icon is not being highlighted so the space between the icon and text was highlighted and looked bad.
    - Added the no-appreciation-icon legend to each shift sign up section.
    - Switched the callsign-list css selector from a flex layout to a column count method.
    - Dayjs was missing the Object Support plugin causing the laborDay() method to fail.
    - Added an explanation on the Copy Slots dialog to what the override fields do.
    - The setup to a create a new position was causing the type field to be blank by not supplying a default.

--- app/components/schedule-position-list.js
+++ app/components/schedule-position-list.js
@@ -1,6 +1,10 @@
 import Component from '@glimmer/component';
 
 export default class SchedulePositionListComponent extends Component {
+  constructor() {
+    super(...arguments);
+    this.haveNoAppreciationSlots = !!this.args.position.slots.find((s) => !s.position_count_hours);
+  }
   get signupCount() {
     return this.args.position.slots.reduce((signups, s) => ((s.person_assigned ? 1 : 0) + signups), 0);
   }
--- app/utils/labor-day.js
+++ app/utils/labor-day.js
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 
 export default function laborDay(year) {
   for (let i = 1; i <= 7; i++) {
-    let day = dayjs({ year, month: 8, day: i});
+    const day = dayjs({ year, month: 8, day: i});
     if (day.day() === 1) { // first Monday of September
       return day;
     }
