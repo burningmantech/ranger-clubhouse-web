7fc83f37b2e62bca1edf5fac230ec4081ec2cd51
mikeburg <mike@burg.me>
2021-08-10 10:54:55 -0700

    Minor UI tweaks.
    - Reduced position form columns down to 4 from 5... thanks ONE positions for throwing the layout off.
    - New Masonry CSS
    - New <PeriodSelect /> component: group a datetime list into days with radio buttons.
    - Shift Lead report using new <PeriodSelect />

--- app/routes/reports/shift-coverage.js
+++ app/routes/reports/shift-coverage.js
@@ -1,10 +1,12 @@
 import ClubhouseRoute from 'clubhouse/routes/clubhouse-route';
 import requestYear from 'clubhouse/utils/request-year';
+import _ from 'lodash';
+import dayjs from 'dayjs';
 
 export default class ReportsShiftCoverageRoute extends ClubhouseRoute {
   queryParams = {
-    year: { refreshModel: true },
-    type: { refreshModel: true },
+    year: {refreshModel: true},
+    type: {refreshModel: true},
   };
 
   model(params) {
@@ -13,15 +15,27 @@ export default class ReportsShiftCoverageRoute extends ClubhouseRoute {
 
     this.year = year;
     this.type = type;
-    return this.ajax.request('slot/shift-coverage-report', { data: { year, type }});
+    return this.ajax.request('slot/shift-coverage-report', {data: {year, type}});
   }
 
   setupController(controller, model) {
-    controller.set('periods', model.periods);
-    controller.set('columns', model.columns);
+    const {periods, columns} = model
+    controller.set('periods', periods);
+    controller.set('columns', columns);
     controller.set('year', this.year);
     controller.set('type', this.type);
     controller.set('dayFilter', 'all');
+
+    periods.forEach((period) => period.monthDay = dayjs(period.date).format('MMDD'));
+
+    controller.set('dayPeriods', _.map(_.groupBy(periods, 'monthDay'), (periods, monthDay) => ({monthDay, periods})));
+
+    const options = _.uniqBy(periods, 'monthDay')
+      .map((p) => ({id: p.monthDay, title: dayjs(p.date).format('ddd MMM D')}));
+
+    options.unshift({id: 'all', title: 'All'});
+
+    controller.set('dayOptions', options);
   }
 
   resetController(controller, isExiting) {
