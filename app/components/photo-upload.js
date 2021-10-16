4a22834fe1aae46387eb627a7bfa0f1b6af3612f
mikeburg <mike@burg.me>
2021-10-15 20:25:15 -0700

    Fixed photo uploading, blueimp-loadimage no longer exports a global function.

--- app/components/photo-upload.js
+++ app/components/photo-upload.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import loadImage from 'blueimp-load-image';
 
 export default class PhotoUploadComponent extends Component {
   @service ajax;
