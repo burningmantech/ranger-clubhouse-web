d22c63d4e1af8ba6eae4d6e2722b63e609fd280f
mikeburg <mike@burg.me>
2022-03-19 17:08:14 -0700

    Addon upgrades, tweaks to the modal dialog component.

--- app/components/modal-dialog.js
+++ app/components/modal-dialog.js
@@ -1,11 +1,17 @@
 import Component from '@glimmer/component';
 import {service} from '@ember/service';
 import {schedule} from '@ember/runloop';
+import { action } from '@ember/object';
 
 export default class ModalDialogComponent extends Component {
   @service modal;
 
   dialogRegistry = {};
+
+  /**
+   * Register a non-inline modal with the modal service. The service handles
+   * the <Escape> key callbacks, and causes the modal to be rendered via modal-registry.js
+   */
 
   constructor() {
     super(...arguments);
@@ -18,6 +24,26 @@ export default class ModalDialogComponent extends Component {
     this.dialogRegistry.onEscape = this.args.onEscape;
     schedule('afterRender', () => this.modal.addDialog(this.dialogRegistry));
   }
+
+  /**
+   * Hack - <BSModal> does not have a fullscreen option, nor a way to add
+   * a class to the inner .modal-dialog div.
+   *
+   * @param {HTMLElement} element
+   */
+
+  @action
+  adjustClassOnInsert(element) {
+    const dialog = element.querySelector('div.modal-dialog');
+    if (!dialog) {
+      return;
+    }
+    dialog.classList.add('modal-fullscreen-md-down');
+  }
+
+  /**
+   * Remove a non-inline dialog from the modal service when destroyed.
+   */
 
   willDestroy() {
     super.willDestroy(...arguments);
