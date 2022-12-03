e764af6c3cd12b979d2e937c71bbee48c58012f1
mikeburg <mike@burg.me>
2022-12-03 12:31:36 -0800

    Radio Eligibility Report adjustments
    - Report on the last 3 events as per policy, not just the last 2.
    - Use the API result to show the years being reported on
    - Indicate what Shift Lead positions are being considered.

--- app/routes/admin/radio-eligibility.js
+++ app/routes/admin/radio-eligibility.js
@@ -10,30 +10,15 @@ export default class AdminRadioEligibilityRoute extends ClubhouseRoute {
     const year = requestYear(params);
     this.year = year;
 
-    return this.ajax.request('timesheet/radio-eligibility', {data: {year}}).then((results) => results.people);
+    return this.ajax.request('timesheet/radio-eligibility', {data: {year}});
   }
 
-  setupController(controller, model) {
+  setupController(controller, {people, year_1, year_2, year_3, shift_lead_positions}) {
     controller.set('year', this.year);
-
-    let year_last, year_prev;
-    switch (this.year) {
-      case 2022:
-        year_last = 2019;
-        year_prev = 2018;
-        break;
-      case 2023:
-        year_last = 2022;
-        year_prev = 2019;
-        break;
-      default:
-        year_last = this.year - 1;
-        year_prev = this.year - 2;
-        break;
-    }
-    controller.set('year_last', year_last);
-    controller.set('year_prev', year_prev);
-    controller.set('people', model);
+    controller.setProperties({
+      people, year_1, year_2, year_3, pandemicSkipped: (year_1 - year_3) > 2,
+      shift_lead_positions
+    });
   }
 
   resetController(controller, isExiting) {
