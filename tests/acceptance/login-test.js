1c81e186eb5fe04ba97821cb34782ea6ca5d7e35
mikeburg <mike@burg.me>
2022-12-26 11:05:52 -0800

    Lots of UI Tweaks (#1538)
    - Fixed UiNotice component so it's responsive.
    - Ensure all UiNotice invocations have a title prop.
    - Improved UiTab rendering on small screens - try to keep the tabs all in one line and prevent multiline wrapping.
    - Improved color contrast on toast messages.
    - Reorganized Person Manager > Event / Training info. Added legal warning above all agreement checkboxes.

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -59,7 +59,7 @@ module('Acceptance | login', function (hooks) {
     // Should stay on the login page
     assert.strictEqual(currentURL(), '/login', 'stay on the login page');
     // And there should be a flash modal with login error
-    assert.dom('.notice-title').hasText(/Login has been disabled/);
+    assert.dom('.notice-title').hasText(/Login disabled/);
     assert.strictEqual(document.title, 'Login | Ranger Clubhouse');
   });
 
