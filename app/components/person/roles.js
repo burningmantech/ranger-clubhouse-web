5e7bc7368b504eb2fd8075d9088d64490a8c8f8d
mikeburg <mike@burg.me>
2023-01-18 13:19:30 -0800

    Support for (ART) training required for assoc. roles to take effect. (#1560)
    - People By Status report indicates if the person passed (ART) training or not.
    - Export To CSV added to People By Status report
    - Edit Roles dialog was not showing team membership role associations.
    - Show Roles redesigned to list the roles as a table.

--- app/components/person/roles.js
+++ app/components/person/roles.js
@@ -9,17 +9,31 @@ export default class PersonRolesComponent extends Component {
 
     this.args.roles.forEach((role) => {
       const granted = !!roles.find((r) => role.id === r.id);
+      const teams = team_roles.filter((t) => t.role_id === role.id).sort((a, b) => a.title.localeCompare(b.title));
+      const positions = position_roles.filter((p) => p.role_id === role.id).sort((a, b) => a.title.localeCompare(b.title));
 
-      if (!granted && !position_roles.find((p) => p.role_id === role.id)) {
+      if (!granted && !positions.length && !teams.length) {
         return;
+      }
+
+       let notGranted = false;
+
+      // See if the role is only granted thru the positions, said positions require training before the roles
+      // are granted, and the person is not (ART) trained.
+      if (!granted
+        && !teams.length
+        && positions.filter((p) => p.require_training_for_roles).length === positions.length
+        && !positions.some((p) => p.is_trained)) {
+        notGranted = true;
       }
 
       foundRoles.push({
         id: role.id,
         title: role.title,
         granted,
-        positions: position_roles.filter((p) => p.role_id === role.id).sort((a, b) => a.title.localeCompare(b.title)),
-        teams: team_roles.filter((t) => t.role_id === role.id).sort((a, b) => a.title.localeCompare(b.title)),
+        positions,
+        teams,
+        notGranted,
       });
     });
 
--- app/controllers/reports/people-by-role.js
+++ app/controllers/reports/people-by-role.js
@@ -3,6 +3,14 @@ import {tracked} from '@glimmer/tracking';
 import {action} from '@ember/object';
 import {cached} from '@glimmer/tracking';
 
+const CSV_COLUMNS = [
+  {title: 'ID', key: 'id'},
+  {title: 'Callsign', key: 'callsign'},
+  {title: 'Explicit Grant?', key: 'granted', yesno: true },
+  {title: 'Teams', key: 'teams' },
+  {title: 'Positions', key: 'positions'}
+];
+
 export default class ReportsPeopleByRoleController extends ClubhouseController {
   @tracked showPerson = null;
   @tracked filter;
@@ -44,4 +52,54 @@ export default class ReportsPeopleByRoleController extends ClubhouseController {
     this.showPerson = null;
   }
 
+  @action
+  exportToCSV(role) {
+    const rows = role.people.map((p) => {
+      return {
+        id: p.id,
+        callsign: p.callsign,
+        granted: p.granted,
+        teams: p.teams.map((t) => t.title).join("\n"),
+        positions: p.positions.map((p) => p.title).join("\n"),
+        positions_granted: p.positions.map((p) => {
+          if (!p.require_training_for_roles) {
+            return 'Granted - Training not required';
+          } else if (p.is_trained) {
+            return 'Granted - Training passed';
+          } else {
+            return 'NOT GRANTED - Training not passed';
+          }
+        }).join("\n")
+      }
+    })
+
+    this.house.downloadCsv(`${this.house.currentYear()}-${role.title}-grants.csv`, CSV_COLUMNS, rows);
+  }
+
+  grantedHow(person) {
+    const grants = [];
+
+    if (!person.granted && !person.teams.length) {
+      // See if the position is only granted thru positions which requires training
+      if (person.positions.filter((p) => p.require_training_for_roles).length === person.positions.length) {
+        if (!person.positions.find((p) => p.is_trained)) {
+          return 'untrained position';
+        }
+      }
+    }
+
+    if (person.granted) {
+      grants.push('role');
+    }
+
+    if (person.positions.length) {
+      grants.push('position');
+    }
+
+    if (person.teams.length) {
+      grants.push('team');
+    }
+
+    return grants.join(', ');
+  }
 }
