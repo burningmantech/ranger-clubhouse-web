32b3b5f918f973a0aa50a760a874691c1a2fd469
mikeburg <mike@burg.me>
2023-01-18 17:52:00 -0800

    People By Role tweaks. (#1562)

--- app/routes/reports/people-by-role.js
+++ app/routes/reports/people-by-role.js
@@ -9,5 +9,17 @@ export default class ReportsPeopleByRoleRoute extends ClubhouseRoute {
     controller.roles = roles;
     controller.filter = 'all';
     controller.showPerson = null;
+
+    roles.forEach((role) => {
+      role.people.forEach((person) => {
+          // See if the role is only assigned thru positions which requires training
+          if (!person.granted && !person.teams.length
+            && person.positions.filter((p) => p.require_training_for_roles).length === person.positions.length
+            && !person.positions.find((p) => p.is_trained)) {
+            person.notAssigned = true;
+          }
+        }
+      );
+    })
   }
 }
