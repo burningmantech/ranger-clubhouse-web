f6a0253181ba9e3492cb63499167a5bf80039b47
mikeburg <mike@burg.me>
2023-04-22 12:32:36 -0700

    Allow render to settle before attempting input focus. (#1631)

--- app/utils/focus-element.js
+++ app/utils/focus-element.js
@@ -1,11 +1,20 @@
 import {schedule} from '@ember/runloop';
 
 export default function focusElement(element) {
+  if (element.offsetParent !== null) {
+    // Element is visible, allow the render to settle, and then focus.
+    setTimeout(() => schedule('afterRender', () => element.focus()), 25);
+    return;
+  }
+
+  // Element is inserted into the DOM however it is not visible yet.
   if (typeof IntersectionObserver !== "undefined") {
+    // Setup to observe when the element becomes visible.
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
         if (entry.intersectionRatio > 0) {
           element.focus();
+          console.log('** ELEMENT FOCUSED - VIA OBSERVER');
           observer.unobserve(element);
         }
       });
