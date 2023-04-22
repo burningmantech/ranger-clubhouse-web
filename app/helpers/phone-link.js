b92f3c5b700e258b1161d8c965fdb3e97073d3a5
mikeburg <mike@burg.me>
2023-04-22 15:05:27 -0700

    Pulled libphonenumber, formatting now done in the backend.

--- app/helpers/phone-link.js
+++ app/helpers/phone-link.js
@@ -1,34 +1,8 @@
 import {helper} from '@ember/component/helper';
 import {htmlSafe} from '@ember/template';
-import {parsePhoneNumberFromString, ParseError} from 'libphonenumber-js'
 
 export function phoneLink([number]) {
-  let phoneURI, phoneFormatted;
-
-  try {
-    const normalized = number.replace(/[^\w]+/g, '');
-    const phone = parsePhoneNumberFromString(number, (normalized.length === 10 || (normalized.length === 11 && normalized.substr(0,1) === '1'))  ? 'US' : undefined);
-    if (phone) {
-      if (phone.country === 'US') {
-        phoneFormatted = phone.formatNational();
-      } else {
-        phoneFormatted = phone.formatInternational();
-      }
-      phoneURI = phone.getURI();
-    } else {
-      phoneFormatted = number;
-      phoneURI = `tel:${number}`;
-    }
-  } catch (error) {
-    if (error instanceof ParseError) {
-     phoneFormatted = number;
-     phoneURI = `tel:${number}`;
-    } else {
-      throw error;
-    }
-  }
-
-  return htmlSafe(`<a href="${phoneURI}">${phoneFormatted}</a>`)
+  return htmlSafe(`<a href="tel:${number}">${number}</a>`)
 }
 
 export default helper(phoneLink);
