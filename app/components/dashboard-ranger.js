15289a66b00c6f7617817aa1f4eca1eb672599bf
mikeburg <mike@burg.me>
2023-05-08 11:46:05 -0700

    Allow Non Rangers to (optionally) take the Online Course.

--- app/components/dashboard-ranger.js
+++ app/components/dashboard-ranger.js
@@ -16,6 +16,7 @@ import {
   WAITING
 } from "clubhouse/constants/dashboard";
 import * as DashboardStep from 'clubhouse/constants/dashboard-steps';
+import {ONLINE_TRAINING} from "clubhouse/constants/dashboard-steps";
 
 const RESULT_PRIORITIES = {
   [URGENT]: 1,
@@ -275,6 +276,7 @@ const NON_RANGER_STEPS = [
   DashboardStep.UPLOAD_PHOTO,
   DashboardStep.PHOTO_APPROVAL,
   DashboardStep.MISSING_BPGUID,
+  DashboardStep.ONLINE_TRAINING,
   DashboardStep.TICKETING_OPEN,
   DashboardStep.TICKETING_CLOSED,
   DashboardStep.VEHICLE_REQUESTS,
