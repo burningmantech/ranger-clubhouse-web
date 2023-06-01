d67df56b30258155fd7a547267bc6d89a0d6695d
mikeburg <mike@burg.me>
2023-06-01 08:49:37 -0700

    Addon upgrades. Bootstrap 5.3 upgrade.

--- app/routes/reports/alpha-shirts.js
+++ app/routes/reports/alpha-shirts.js
@@ -1,9 +1,9 @@
 import ClubhouseRoute from 'clubhouse/routes/clubhouse-route';
-import {ADMIN, VC} from 'clubhouse/constants/roles';
+import {MANAGE} from 'clubhouse/constants/roles';
 import shirtGroupsSort from 'clubhouse/utils/shirt-groups-sort';
 
 export default class ReportsAlphaShirtsRoute extends ClubhouseRoute {
-  roleRequired = [ADMIN, VC];
+  roleRequired = [MANAGE];
 
   model() {
     return this.ajax.request(`person/alpha-shirts`).then((result) => result.alphas);
