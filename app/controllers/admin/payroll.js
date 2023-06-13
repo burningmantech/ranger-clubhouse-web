3594500fcdb6e400a47e7886fee50ddc645db07e
mikeburg <mike@burg.me>
2023-06-13 14:25:36 -0700

    Payroll report changes. (#1697)

--- app/controllers/admin/payroll.js
+++ app/controllers/admin/payroll.js
@@ -4,12 +4,18 @@ import validateDateTime from "clubhouse/validators/datetime";
 import {action} from '@ember/object';
 import {tracked} from '@glimmer/tracking';
 import {hourMinuteFormat} from "clubhouse/helpers/hour-minute-format";
+import dayjs from 'dayjs';
+
+const DEPARTMENT_CODE = '660';  // the Ranger department code
 
 const CSV_TABLE_COLUMNS = [
   {title: 'Callsign', key: 'callsign'},
   {title: 'First Name', key: 'first_name'},
   {title: 'Last Name', key: 'last_name'},
+  {title: 'Employee ID', key: 'employee_id'},
   {title: 'Position', key: 'position_title'},
+  {title: 'Paycode', key: 'code'},
+  {title: 'Entry Status', key: 'status'},
   {title: 'Orig Start', key: 'orig_on_duty'},
   {title: 'Orig End', key: 'orig_off_duty'},
   {title: 'Orig Duration', key: 'orig_duration'},
@@ -20,6 +26,26 @@ const CSV_TABLE_COLUMNS = [
   {title: 'Second Half End', key: 'second_off_duty'},
   {title: 'Total Adj. Duration', key: 'adj_duration'},
   {title: 'Notes', key: 'notes'}
+];
+
+const PUNCH_IN_DAY = 'ID';
+const PUNCH_OUT_DAY = 'OD';
+const PUNCH_IN_LUNCH = 'IL';
+const PUNCH_OUT_LUNCH = 'OL';
+
+const PAYROLL_CSV = [
+  {title: 'Employee No.', key: 'employee_id'},
+  {title: 'Department', value: DEPARTMENT_CODE},
+  {title: 'Date', key: 'date'},
+  {title: 'Punch Time', key: 'time'},
+  {title: 'Punch Type', key: 'type'},
+  {title: 'Rate Type', blank: true},
+  {title: 'Leave Blank', blank: true},
+  {title: 'Punch Comment', key: 'comment'},
+  {title: 'Sub-Department', key: 'paycode'},
+  {title: 'Leave Blank', blank: true},
+  {title: 'Leave Blank', blank: true},
+  {title: 'Rate', blank: true},
 ];
 
 export default class AdminPayrollController extends ClubhouseController {
@@ -62,13 +88,32 @@ export default class AdminPayrollController extends ClubhouseController {
     ["16 hours", 16],
   ];
 
+  /**
+   * How many total entries are being shown?
+   *
+   * @returns {Number}
+   */
+
   get entryCount() {
     return this.people.reduce((total, person) => total + person.shifts.length, 0);
   }
 
+  /**
+   * Generate a payroll report to show.
+   *
+   * @param {*} model
+   * @param {boolean} isValid
+   * @returns {Promise<void>}
+   */
+
   @action
   async submitReport(model, isValid) {
     if (!isValid) {
+      return;
+    }
+
+    if (!model.position_ids.length) {
+      this.modal.info('No positions selected', 'Please select one or more positions to report on.');
       return;
     }
 
@@ -82,6 +127,7 @@ export default class AdminPayrollController extends ClubhouseController {
           end_time: model.end_time,
           break_duration: model.break_duration,
           hour_cap: model.hour_cap,
+          position_ids: model.position_ids,
         }
       });
       this.people = people;
@@ -93,6 +139,10 @@ export default class AdminPayrollController extends ClubhouseController {
     }
   }
 
+  /**
+   * Export the raw table
+   */
+
   @action
   exportTable() {
     const rows = [];
@@ -102,15 +152,23 @@ export default class AdminPayrollController extends ClubhouseController {
           'callsign': person.callsign,
           'first_name': person.first_name,
           'last_name': person.last_name,
+          'employee_id': person.employee_id,
           'position_title': entry.position_title,
-          'position_code': entry.position_code,
+          'code': entry.paycode,
           'orig_on_duty': entry.on_duty,
           'orig_off_duty': entry.off_duty,
           'orig_duration': hourMinuteFormat([entry.orig_duration]),
           'adj_duration': hourMinuteFormat([entry.duration]),
           notes: entry.notes,
         };
-        rows.push(row);
+
+        if (entry.still_on_duty) {
+          row.status = 'Still On Duty';
+        } else if (entry.verified) {
+          row.status = 'Verified';
+        } else {
+          row.status = 'Unverified';
+        }
 
         const adj = entry.meal_adjusted;
         if (adj) {
@@ -123,14 +181,76 @@ export default class AdminPayrollController extends ClubhouseController {
           row.first_on_duty = entry.on_duty;
           row.first_off_duty = entry.off_duty;
         }
+
+        rows.push(row);
       })
     });
 
-    return this.house.downloadCsv(`${this.house.currentYear()}-payroll.csv`, CSV_TABLE_COLUMNS, rows);
+    this.house.downloadCsv(`${this.house.currentYear()}-payroll-raw.csv`, CSV_TABLE_COLUMNS, rows);
   }
 
-  @action
-  exportUnadjustPayroll() {
+  /**
+   * Export the payroll spreadsheet with the adjusted times, or unadjusted.
+   *
+   * @param adjustEntries
+   */
 
+  @action
+  exportPayroll(adjustEntries) {
+    const rows = [];
+
+    this.people.forEach((person) => {
+      person.shifts.forEach((entry) => {
+        const adj = entry.meal_adjusted;
+        if (adjustEntries) {
+          if (adj) {
+            // The entry has meals breaks.
+            this._punchTheClock(person, rows, entry, adj.first_half.on_duty, adj.first_half.off_day, PUNCH_IN_DAY, PUNCH_IN_LUNCH);
+            this._punchTheClock(person, rows, entry, adj.second_half.on_duty, adj.second_half.off_day, PUNCH_OUT_LUNCH, PUNCH_OUT_DAY);
+          } else {
+            // No meal breaks, although entry might have a daily hour cap applied.
+            this._punchTheClock(person, rows, entry, entry.on_duty, entry.off_duty, PUNCH_IN_DAY, PUNCH_OUT_DAY);
+          }
+        } else {
+          // Export the original timesheet entry as-is.
+          this._punchTheClock(person, rows, entry, entry.orig_on_duty, entry.orig_off_duty, PUNCH_IN_DAY, PUNCH_OUT_DAY);
+        }
+      });
+    });
+
+    this.house.downloadCsv(`${this.house.currentYear()}-payroll-${adjustEntries ? 'adjusted' : 'unadjusted'}.csv`, PAYROLL_CSV, rows);
+  }
+
+  /**
+   * Create a spreadsheet row with the given date-times, and punch codes.
+   *
+   * @param {*} person
+   * @param {[]} rows
+   * @param {*} entry
+   * @param {string} onDuty
+   * @param {string} offDuty
+   * @param {string} inCode
+   * @param {string} outCode
+   * @private
+   */
+
+  _punchTheClock(person, rows, entry, onDuty, offDuty, inCode, outCode) {
+    const onDutyDT = dayjs(onDuty), offDutyDT = dayjs(offDuty);
+
+    rows.push({
+      employee_id: person.employee_id,
+      date: onDutyDT.format('YYYY-MM-DD'),
+      time: onDutyDT.format('HH:mm'),
+      type: inCode,
+      paycode: entry.paycode,
+      comment: `${person.callsign}\n${entry.position_title}`
+    });
+
+    rows.push({
+      employee_id: person.employee_id,
+      date: offDutyDT.format('YYYY-MM-DD'),
+      time: offDutyDT.format('HH:mm'),
+      type: outCode,
+    });
   }
 }
