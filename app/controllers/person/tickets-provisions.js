78c750b37e8f0bb80cd6c5567259357b31ef3ab8
mikeburg <mike@burg.me>
2023-06-21 18:02:18 -0700

    Include BMID provision when showing the effective provisions on Person Manage > Tickets Provisions (#1713)

--- app/controllers/person/tickets-provisions.js
+++ app/controllers/person/tickets-provisions.js
@@ -1,6 +1,6 @@
 import ClubhouseController from 'clubhouse/controllers/clubhouse-controller';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
+import {action} from '@ember/object';
+import {tracked} from '@glimmer/tracking';
 
 export default class PersonTicketsProvisionsController extends ClubhouseController {
   @tracked isLoading = false;
@@ -8,17 +8,29 @@ export default class PersonTicketsProvisionsController extends ClubhouseControll
   @tracked ticketingInfo;
   @tracked ticketingPackage;
   @tracked provisions;
+  @tracked bmid;
 
   @action
   async loadStuff() {
     this.isLoading = true;
-    const data = { person_id: this.person.id };
+    const data = {person_id: this.person.id};
 
     try {
       this.documents = await this.store.query('access-document', data);
       this.provisions = await this.store.query('provision', data);
-      this.ticketingInfo = await this.ajax.request(`ticketing/info`).then(({ticketing_info}) => ticketing_info);
-      this.ticketingPackage = await this.ajax.request(`ticketing/${this.person.id}/package`).then((results) => results.package);
+      this.ticketingInfo = (await this.ajax.request(`ticketing/info`)).ticketing_info;
+      this.ticketingPackage = (await this.ajax.request(`ticketing/${this.person.id}/package`)).package;
+      const {bmid} = await this.ajax.request(`bmid/manage-person`, {
+        data: {
+          person_id: this.person.id,
+          year: this.house.currentYear()
+        }
+      });
+      if (bmid.id) {
+        this.bmid = this.house.pushPayload('bmid', bmid);
+      } else {
+        this.bmid = this.store.createRecord('bmid', bmid);
+      }
     } catch (response) {
       this.house.handleErrorResponse(response);
     } finally {
