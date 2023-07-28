ed840a656fa3520948ce0edd3b8a0471874b4c53
mikeburg <mike@burg.me>
2023-07-28 10:47:23 -0700

    To combat playa brain, combine the save & next buttons on the site check-in dialogs. (#1759)

--- app/components/ui-wizard-step.js
+++ app/components/ui-wizard-step.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import {tracked} from '@glimmer/tracking';
+import {action} from '@ember/object';
 
 export default class UiWizardStepComponent extends Component {
   @tracked isActive = false;
@@ -15,4 +16,33 @@ export default class UiWizardStepComponent extends Component {
     this.shortTitle = shortTitle;
     this.wizard.registerStep(this);
   }
+
+  get nextLabel() {
+    return this.args.nextLabel ?? this.wizard.nextLabel;
+  }
+
+  @action
+  nextAction() {
+    const {nextAction} = this.args;
+    if (nextAction) {
+      nextAction(this.wizard.nextStep);
+    } else {
+      this.wizard.nextStep();
+    }
+  }
+
+  get finishLabel() {
+    return this.args.finishLabel ?? this.wizard.finishLabel;
+  }
+
+  @action
+  finishAction() {
+    const {finishAction} = this.args;
+    if (finishAction) {
+      finishAction(this.wizard.finishAction);
+    } else {
+      this.wizard.finishAction();
+    }
+  }
+
 }
