a4cefc295933f2719fe5c0ef923357cd0491d174
mikeburg <mike@burg.me>
2023-11-28 16:18:55 -0800

    Call replaceWith in its new location. Meh. (#1878)

--- app/routes/client.js
+++ app/routes/client.js
@@ -1,11 +1,14 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class ClientRoute extends Route {
+  @service router;
+
   beforeModel() {
     let uri = location.pathname.replace(/^\/client\/?/, '/');
     if (location.search) {
       uri += location.search;
     }
-    this.replaceWith(uri);
+    this.router.replaceWith(uri);
   }
 }
