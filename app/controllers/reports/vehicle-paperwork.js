4f2a0f5e5e6911f7982ce243fabcad710dabb5d3
mikeburg <mike@burg.me>
2024-01-10 18:22:21 -0800

    Added MVR columns to Vehicle Paperwork export.

--- app/controllers/reports/vehicle-paperwork.js
+++ app/controllers/reports/vehicle-paperwork.js
@@ -9,9 +9,20 @@ export default class ReportsVehiclePaperworkController extends ClubhouseControll
         { title: 'Callsign', key: 'callsign' },
         { title: 'Status', key: 'status' },
         { title: `${year} Motorpool Agreement`, key: 'signed_motorpool_agreement', yesno: true },
-        { title: `${year} Motor Vehicle Record (MVR)`, key: 'org_vehicle_insurance', yesno: true },
+        { title: `${year} MVR Approved`, key: 'org_vehicle_insurance', yesno: true },
+        { title: `${year} MVR Eligible`, key: 'is_mvr_eligible', yesno: true },
+        { title: `${year} MVR Checkbox`, key: 'mvr_eligible', yesno: true },
+        { title: `MVR Teams`, key: 'teams' },
+        { title: `MVR Positions`, key: 'positions'},
       ];
 
-      this.house.downloadCsv(`${year}-vehicle-paperwork.csv`, CSV_COLUMNS, this.people);
+      const people = this.people.map((p) => ({
+        ...p,
+        is_mvr_eligible: p.mvr_eligible || p.mvr_teams?.length || p.mvr_positions?.length,
+        teams: p.mvr_teams ? p.mvr_teams.map((t) => t.title).join("\n") : '',
+        positions: p.mvr_positions ? p.mvr_positions.map((t) => t.title).join("\n") : '',
+      }));
+
+      this.house.downloadCsv(`${year}-vehicle-paperwork.csv`, CSV_COLUMNS, people);
     }
 }
