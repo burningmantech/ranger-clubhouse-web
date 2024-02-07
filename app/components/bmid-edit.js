c1cbb5a1a0ef0b7f8e87e49af1c64d7b23b0200e
mikeburg <mike@burg.me>
2024-02-06 18:22:33 -0800

    Support new automatic BMID printing qualifications

--- app/components/bmid-edit.js
+++ app/components/bmid-edit.js
@@ -9,4 +9,9 @@ export default class BmidEditComponent extends Component {
   bmidStatusOptions = BmidStatusOptions;
   mealOptions = MealOptions;
   showerOptions = ShowerOptions;
+
+  get notQualified() {
+    const {entry} = this.args;
+    return !entry.has_ticket && !entry.training_signed_up;
+  }
 }
