88e28fbf4710c51548ba6d72bd4e935b46a26c4b
mikeburg <mike@burg.me>
2024-04-15 11:50:01 -0700

    Some Error Log formatting work. (#2004)
    * Some Error Log formatting work.
    
    * Lint fix

--- app/controllers/admin/error-log.js
+++ app/controllers/admin/error-log.js
@@ -1,6 +1,6 @@
 import ClubhouseController from 'clubhouse/controllers/clubhouse-controller';
-import { set } from '@ember/object';
-import { action } from '@ember/object';
+import {set} from '@ember/object';
+import {action} from '@ember/object';
 
 export default class AdminErrorLogController extends ClubhouseController {
 
@@ -20,9 +20,29 @@ export default class AdminErrorLogController extends ClubhouseController {
   @action
   purgeLog() {
     this.modal.confirm('Delete All Error Logs', 'Are you sure you wish to delete the entire error log?', () => {
-      this.ajax.request('error-log/purge', { method: 'DELETE' })
-      .then(() => this.send('refreshRoute'))
-      .catch((response) => this.house.handleErrorResponse(response));
+      this.ajax.request('error-log/purge', {method: 'DELETE'})
+        .then(() => this.send('refreshRoute'))
+        .catch((response) => this.house.handleErrorResponse(response));
     })
+  }
+
+  @action
+  isClientException(log) {
+    return log.error_type === 'client-ember-error' || log.error_type === 'client-ember-route-error';
+  }
+
+  @action
+  exceptionDetails(data) {
+    try {
+      const parsed = JSON.parse(data);
+      const details = JSON.parse(parsed.details);
+      return {
+        exceptionName: parsed.name,
+        exceptionMessage: parsed.exception,
+        stack: details.stack,
+      }
+    } catch (SyntaxError) {
+      return data;
+    }
   }
 }
