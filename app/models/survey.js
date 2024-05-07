174b90f2e9fdcbc840d6e2625c467b79a7ef182f
mikeburg <mike@burg.me>
2024-05-06 22:03:45 -0700

    Added survey active flag. Use WYSIWYG editor to epilogue & prologue text.

--- app/models/survey.js
+++ app/models/survey.js
@@ -1,10 +1,11 @@
 import Model, {attr} from '@ember-data/model';
 
 export const TYPE_TRAINER = 'trainer';
-export const TYPE_TRAINING  = 'training';
+export const TYPE_TRAINING = 'training';
 export const TYPE_ALPHA = 'alpha';
 
 export default class SurveyModel extends Model {
+  @attr('boolean') active;
   @attr('number') year;
   @attr('string') type;
   @attr('number') position_id;
