9b9a87e29cb56829782a9c418303c8f3f681bc83
mikeburg <frankenstein@burningman.org>
2024-06-23 11:05:18 -0700

    Converted badge helper to UiBadge component. (#2055)

--- app/helpers/badge.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-import { htmlSafe } from '@ember/template';
-
-export function badge([ badgeType, text ]) {
-  return htmlSafe(`<span class="badge text-bg-${badgeType}">${text}</span>`)
-}
-
-export default helper(badge);
