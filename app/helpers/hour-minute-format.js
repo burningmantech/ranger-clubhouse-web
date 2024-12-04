a459a4bc0cc8b0dae802dbcc85e0aa9bb12abe5c
mikeburg <frankenstein@burningman.org>
2024-12-03 20:11:46 -0800

    Some ARIA work. (#2146)
    * Some ARIA work.
    
    * Fixed lint errors.
    
    * Fixed test.

--- app/helpers/hour-minute-format.js
+++ app/helpers/hour-minute-format.js
@@ -1,18 +1,18 @@
-import { helper } from '@ember/component/helper';
+import {helper} from '@ember/component/helper';
+import {htmlSafe} from '@ember/template';
 
-export function hourMinuteFormat([ duration ]) {
+export function hourMinuteFormat([duration]) {
   if (!duration) {
     duration = 0;
   }
 
   const minutes = Math.floor((duration / 60) % 60);
-  const hours =  Math.floor(duration / 3600);
+  const hours = Math.floor(duration / 3600);
 
-  if (minutes < 10) {
-    return `${hours}:0${minutes}`;
-  } else {
-    return `${hours}:${minutes}`;
-  }
+  const label = minutes < 10 ? `${hours}:0${minutes}` : `${hours}:${minutes}`;
+  const minLabel = (minutes === 1) ? `1 minute` : `${minutes} minutes`;
+  const datetime = hours ? `${hours} hours ${minLabel}` :minLabel;
+  return htmlSafe(`<span aria-hidden="true">${label}</span><span class="sr-only">${datetime}</span>`);
 }
 
 export default helper(hourMinuteFormat);
