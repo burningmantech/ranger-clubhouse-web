07714bd94617940d914ad3839a62a1ff84bf3304
mikeburg <mike@burg.me>
2025-03-24 23:14:07 -0700

    More addon upgrades

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,12 +1,13 @@
 import Application from 'clubhouse/app';
 import config from 'clubhouse/config/environment';
 import * as QUnit from 'qunit';
-import { setApplication } from '@ember/test-helpers';
-import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
+import {setApplication} from '@ember/test-helpers';
+import {setup} from 'qunit-dom';
+import {start} from 'ember-qunit';
+import {loadTests} from 'ember-qunit/test-loader';
 
 setApplication(Application.create(config.APP));
-
 setup(QUnit.assert);
+loadTests()
 
 start();
