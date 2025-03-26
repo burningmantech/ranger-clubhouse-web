5e48f189ad131330040c45bdee857575e3e33ecd
mikeburg <frankenstein@burningman.org>
2025-03-26 12:10:59 -0700

    Adjust the "online course not ready" message based on status. (#2206)

--- app/components/schedule-blocked.js
+++ app/components/schedule-blocked.js
@@ -1,14 +1,8 @@
 import Component from '@glimmer/component';
-import { REQUIREMENT_LABELS } from 'clubhouse/constants/signup-requirements';
-import { service } from '@ember/service';
+import {service} from '@ember/service';
 
 export default class ScheduleBlockedComponent extends Component {
   @service session;
-
-  constructor() {
-    super(...arguments);
-    this.requirementsList = this.args.requirements.map((r) => (REQUIREMENT_LABELS[r] || `Unknown code ${r}. Report this bug to the tech team.`));
-  }
 
   get isMe() {
     return this.session.userId == this.person.id;
--- app/components/schedule-requirements.js
+++ app/components/schedule-requirements.js
@@ -1,11 +1,10 @@
 import Component from '@glimmer/component';
-import { REQUIREMENT_LABELS } from "clubhouse/constants/signup-requirements";
+import {requirementInfo} from 'clubhouse/constants/signup-requirements';
 
 export default class ScheduleRequirementsComponent extends Component {
   constructor() {
     super(...arguments);
-    const {requirements} =   this.args;
-    this.requirementsList = requirements.map((req) => REQUIREMENT_LABELS[req]);
-    this.justTrainingBlocker =  (!this.requirementsList.length && this.args.hasTrainingBlocker)
+    this.requirementsList = requirementInfo(this.args.person.status, this.args.requirements);
+    this.justTrainingBlocker = (!this.requirementsList.length && this.args.hasTrainingBlocker)
   }
 }
