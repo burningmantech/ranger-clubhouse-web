e5be86d75ca8bc7329713e607424c0f03e09fe30
mikeburg <mike@burg.me>
2025-03-29 22:01:25 -0700

    More LMYR swatting.
    This time for sure!

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -23,7 +23,7 @@ export default class ApplicationController extends ClubhouseController {
     const isMac = navigator.userAgent.indexOf("Mac") !== -1;
 
     document.querySelector('body').addEventListener('keydown', (event) => {
-      if (!this.session.user || !this.session.hasLoginManagement) {
+      if (!this.session.user || !this.session.hasEventManagement) {
         // Not logged in or does not have EMOP
         return true;
       }
