84c249ed37ad2f589865aeecf3f5693fb6f29601
mikeburg <mike@burg.me>
2025-05-21 18:35:54 -0700

    Support hover over for date times showing the date in the user's timezone.

--- app/helpers/shift-format.js
+++ app/helpers/shift-format.js
@@ -1,18 +1,29 @@
 import {helper} from '@ember/component/helper';
 import dayjs from 'dayjs';
-import { htmlSafe } from '@ember/template';
+import {htmlSafe} from '@ember/template';
 
 export const MONTH_DAY_TIME = 'ddd MMM DD [@] HH:mm';
 export const MONTH_DAY_TIME_YEAR = 'ddd MMM DD [@] HH:mm YYYY';
 export const HOUR_MINS = 'HH:mm';
 export const HOUR_MIN_DAY = 'ddd [@] HH:mm'
 
+const guessedTz = dayjs.tz.guess();
+
 export function shiftFormat([shiftDate, endDate], hash) {
   let datetime;
-  let tz = hash.timezone;
+  let {tz, tzabbr} = hash;
 
   if (!shiftDate) {
     return '';
+  }
+
+  if (!tz) {
+    tz = 'America/Los_Angeles';
+    tzabbr = '';
+  } else if (tzabbr === 'PDT' || tzabbr === 'PST') {
+    tzabbr = '';
+  } else {
+    tzabbr = ` (${tzabbr})`;
   }
 
   datetime = dayjs(shiftDate);
@@ -20,24 +31,20 @@ export function shiftFormat([shiftDate, endDate], hash) {
     return shiftDate;
   }
 
-  if (!tz || tz ==='PST' || tz === 'PDT') {
-    tz = '';
-  } else {
-    tz = ` (${tz})`;
-  }
-
+  const startLocal = dayjs.tz(shiftDate, tz).tz(guessedTz).format(MONTH_DAY_TIME_YEAR + ' (z)');
   if (!endDate) {
-    return `${datetime.format(hash.year ? MONTH_DAY_TIME_YEAR : MONTH_DAY_TIME)}${tz}`;
+    return htmlSafe(`<span title="${startLocal}">${datetime.format(hash.year ? MONTH_DAY_TIME_YEAR : MONTH_DAY_TIME)}${tzabbr}</span>`);
   }
 
   const endDatetime = dayjs(endDate);
   if (!endDatetime.isValid()) {
-    return `${datetime.format(MONTH_DAY_TIME)}${tz}`;
+    return htmlSafe(`<span title="${startLocal}">${datetime.format(MONTH_DAY_TIME)}${tzabbr}</span> to ${endDate}`);
 
   }
 
+  const endLocal = dayjs.tz(endDate, tz).tz(guessedTz).format(MONTH_DAY_TIME_YEAR + ' (z)');
   if (hash.year) {
-    return `${datetime.format(MONTH_DAY_TIME_YEAR)} to ${endDatetime.format(MONTH_DAY_TIME_YEAR)}${tz}`;
+    return htmlSafe(`<span title="${startLocal}">${datetime.format(MONTH_DAY_TIME_YEAR)}${tzabbr}</span> to <span title="${endLocal}">${endDatetime.format(MONTH_DAY_TIME_YEAR)}</span>`);
   }
 
   const startDay = datetime.date(), endDay = endDatetime.date();
@@ -45,7 +52,7 @@ export function shiftFormat([shiftDate, endDate], hash) {
   let endFormat = null;
 
   if (datetime.year() !== endDatetime.year()) {
-    return htmlSafe(`${datetime.format(MONTH_DAY_TIME_YEAR)} to <span class="d-inline-block">${endDatetime.format(MONTH_DAY_TIME_YEAR)}${tz}</span>`);
+    return htmlSafe(`<span title="${startLocal}>${datetime.format(MONTH_DAY_TIME_YEAR)}${tzabbr}</span> to <span class="d-inline-block" title="${endLocal}">${endDatetime.format(MONTH_DAY_TIME_YEAR)}</span>`);
   } else if (startDay === endDay) {
     // Don't bother with added the day on the end time if its only to midnight
     endFormat = endDatetime.format(HOUR_MINS);
@@ -54,7 +61,7 @@ export function shiftFormat([shiftDate, endDate], hash) {
   } else {
     endFormat = endDatetime.format(HOUR_MIN_DAY);
   }
-  return htmlSafe(`${datetime.format(MONTH_DAY_TIME)} to <span class="d-inline-block">${endFormat}${tz}</span>`);
+  return htmlSafe(`<span title="${startLocal}">${datetime.format(MONTH_DAY_TIME)}${tzabbr}</span> to <span class="d-inline-block" title="${endLocal}">${endFormat}</span>`);
 }
 
 export default helper(shiftFormat);
