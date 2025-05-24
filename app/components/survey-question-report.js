6dd325199c1cbb531115546aa1cc6efa2af378e8
mikeburg <frankenstein@burningman.org>
2025-05-24 14:46:28 -0700

    Deanonymize survey reports if the user has the appropriate permissions. (#2260)

--- app/components/survey-question-report.js
+++ app/components/survey-question-report.js
@@ -1,20 +1,7 @@
 import Component from '@glimmer/component';
-import {action, set} from '@ember/object';
-import {TYPE_TRAINING, TYPE_TRAINER} from "clubhouse/models/survey";
 
 export default class SurveyQuestionReportComponent extends Component {
-  constructor() {
-    super(...arguments);
-    this.showCallsign = (this.args.survey.type !== TYPE_TRAINER && this.args.survey.type !== TYPE_TRAINING);
-  }
-
-  @action
-  showPersonAnswerAction(answer, event) {
-    event.preventDefault();
-    set(answer, 'showPerson', true);
-  }
-
-  progressValue(mean) {
+   progressValue(mean) {
     if (!mean) {
       return 0;
     }
