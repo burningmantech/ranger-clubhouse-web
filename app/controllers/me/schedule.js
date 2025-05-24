96a384416f5be64c1e6b8ce6169f31768dba2870
mikeburg <mike@burg.me>
2025-05-23 20:09:22 -0700

    Added year selector to Me > Schedule

--- app/controllers/me/schedule.js
+++ app/controllers/me/schedule.js
@@ -1,9 +1,17 @@
 import ClubhouseController from 'clubhouse/controllers/clubhouse-controller';
 import { tracked } from '@glimmer/tracking';
+import {action} from '@ember/object';
 
 export default class MeScheduleController extends ClubhouseController {
+  queryParams = ['year'];
   @tracked permission;
   @tracked credits_earned;
   @tracked scheduleSummary;
+  @tracked year;
 
+  @action
+  onYearChange(callback, year) {
+    this.year = year;
+    callback?.(year);
+  }
 }
