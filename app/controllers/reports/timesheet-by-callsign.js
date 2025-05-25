05b3c52b29070afd0c225524eea83769e7510a01
mikeburg <mike@burg.me>
2025-05-25 11:24:56 -0700

    Show assoc. slot (if any) on Timesheet By {Callsign,Position} reports.

--- app/controllers/reports/timesheet-by-callsign.js
+++ app/controllers/reports/timesheet-by-callsign.js
@@ -1,6 +1,20 @@
 import ClubhouseController from 'clubhouse/controllers/clubhouse-controller';
 import {action} from '@ember/object';
-import {tracked}from '@glimmer/tracking';
+import {tracked} from '@glimmer/tracking';
+
+const CSV_COLUMNS = [
+  {title: 'Callsign', key: 'callsign'},
+  {title: 'Status', key: 'status'},
+  {title: 'From', key: 'on_duty'},
+  {title: 'To', key: 'off_duty'},
+  {title: 'Hours', key: 'hours'},
+  {title: 'Credits', key: 'credits'},
+  {title: 'Position', key: 'position'},
+  {title: 'Count Hours', key: 'count_hours'},
+  {title: 'Shift Begins', key: 'shift_begins'},
+  {title: 'Shift Duration', key: 'shift_duration'},
+  {title: 'Shift Description', key: 'shift_description'},
+];
 
 export default class ReportsTimesheetByCallsignController extends ClubhouseController {
   queryParams = ['year'];
@@ -16,35 +30,35 @@ export default class ReportsTimesheetByCallsignController extends ClubhouseContr
   }
 
   @action
-  exportToCSV() {
-    const CSV_COLUMNS = [
-      {title: 'Callsign', key: 'callsign'},
-      {title: 'Status', key: 'status'},
-      {title: 'From', key: 'on_duty'},
-      {title: 'To', key: 'off_duty'},
-      {title: 'Hours', key: 'hours'},
-      {title: 'Credits', key: 'credits'},
-      {title: 'Position', key: 'position'},
-      {title: 'Count Hours', key: 'count_hours'}
-    ];
-
+  exportAllToCSV() {
     const rows = [];
+    this.people.forEach((person) => this._buildTimesheetExport(rows, person));
+    this.house.downloadCsv(`${this.year}-timesheet-by-callsign.csv`, CSV_COLUMNS, rows);
+  }
 
-    this.people.forEach((person) => {
-      person.timesheet.forEach((entry) => {
-        rows.push({
-          'callsign': person.callsign,
-          'status': person.status,
-          'on_duty': entry.on_duty,
-          'off_duty': entry.off_duty,
-          'hours': (entry.duration / 3600.0).toFixed(2),
-          'credits': entry.credits.toFixed(2),
-          'position': entry.position.title,
-          'count_hours': entry.position.count_hours ? 'Y' : 'N'
-        });
+  @action
+  exportPersonToCSV(person) {
+    const rows = [];
+    this._buildTimesheetExport(rows, person);
+    this.house.downloadCsv(`${this.year}-${person.callsign.replace(/ /g, '-')}-timesheet.csv`, CSV_COLUMNS, rows);
+  }
+
+  @action
+  _buildTimesheetExport(rows, person) {
+    person.timesheet.forEach((entry) => {
+      rows.push({
+        'callsign': person.callsign,
+        'status': person.status,
+        'on_duty': entry.on_duty,
+        'off_duty': entry.off_duty,
+        'hours': (entry.duration / 3600.0).toFixed(2),
+        'credits': entry.credits.toFixed(2),
+        'position': entry.position.title,
+        'count_hours': entry.position.count_hours ? 'Y' : 'N',
+        'shift_begins': entry.slot?.begins,
+        'shift_duration': entry.slot ? (entry.slot.duration / 3600.0).toFixed(2) : '',
+        'shift_description': entry.slot?.description,
       });
     });
-
-    this.house.downloadCsv(`${this.year}-timesheet-by-callsign.csv`, CSV_COLUMNS, rows);
   }
 }
