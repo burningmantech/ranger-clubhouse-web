45517994325a13b213f6517e5736a2ff32f0003c
mikeburg <mike@burg.me>
2025-05-28 21:08:48 -0700

    Convert Forced Shift Report to use new blocker format.

--- app/controllers/reports/forced-signins.js
+++ app/controllers/reports/forced-signins.js
@@ -1,6 +1,8 @@
 import ClubhouseController from "clubhouse/controllers/clubhouse-controller";
 import {action} from '@ember/object';
-import { tracked } from '@glimmer/tracking';
+import {tracked} from '@glimmer/tracking';
+import {buildBlockerAuditLabels} from "clubhouse/models/timesheet";
+import {htmlSafe} from '@ember/template';
 
 const CSV_COLUMNS = [
   {title: 'Callsign', key: 'callsign'},
@@ -19,5 +21,9 @@ export default class ReportsForcedSigninsController extends ClubhouseController 
   @action
   exportToCSV() {
     this.house.downloadCsv(`${this.year}-forced-signins.csv`, CSV_COLUMNS, this.entries);
+  }
+
+  forceReasons(blockers) {
+    return htmlSafe(buildBlockerAuditLabels(blockers).map(blocker => `<div >${blocker}</div>`).join(''));
   }
 }
