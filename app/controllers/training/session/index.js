95fc20c6d50156999886e289a87f406d64da75a1
mikeburg <frankenstein@burningman.org>
2025-05-30 21:05:46 -0700

    Tell non-cadre ART trainers to contact the cadre after passing ART prospectives to grant mentee positions. (#2270)

--- app/controllers/training/session/index.js
+++ app/controllers/training/session/index.js
@@ -8,7 +8,7 @@ import {isEmpty} from '@ember/utils';
 import _ from 'lodash';
 import conjunctionFormat from 'clubhouse/utils/conjunction-format';
 import {AUDITOR, PROSPECTIVE} from "clubhouse/constants/person_status";
-import {ART_GRADUATE_BASE} from "clubhouse/constants/roles";
+import {ADMIN, ART_GRADUATE_BASE} from "clubhouse/constants/roles";
 
 const SEARCH_RATE_MS = 300;
 
@@ -193,7 +193,7 @@ export default class TrainingSessionController extends ClubhouseController {
   }
 
   get canGraduate() {
-    return this.training.graduation_info && this.session.hasRole(ART_GRADUATE_BASE | this.training.id);
+    return this.training.graduation_info && this.session.hasRole([ADMIN, ART_GRADUATE_BASE | this.training.id]);
   }
 
   /**
