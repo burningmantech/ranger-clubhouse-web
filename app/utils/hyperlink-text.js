c4d81e6e010cb35c6c5339d1265b9afb1ad876d3
mikeburg <frankenstein@burningman.org>
2025-06-09 22:35:04 -0700

    Minor UI tweaks (#2284)
    - Increase horizontal padding between address elements on the Person Manage landing page.
    - hyperLinkText() was not replacing newlines with br tags. (slot descriptions looked a little funny as a result)
    - Change the banner message color from red to yellow.

--- app/utils/hyperlink-text.js
+++ app/utils/hyperlink-text.js
@@ -1,4 +1,6 @@
 export default function hyperlinkText(text) {
-  text = text.replace(/\b(https?:\/\/[^\s]+)/g, '<a href="$1" target="_blank">$1</a>')
-  return text.replace(/\b([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,})\b/gi, '<a href="mailto:$1">$1</a>');
+  return text.replace(/\b(https?:\/\/[^\s]+)/g, '<a href="$1" target="_blank">$1</a>')
+    .replace(/\b([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,})\b/gi, '<a href="mailto:$1">$1</a>')
+    .replace(/(\r\n|\r|\n){2,}$/g, '$1\n')
+    .replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br>$2');
 }
