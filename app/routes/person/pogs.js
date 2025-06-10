a6dc11fd333374a1d1b134b31636c77d70c95d90
mikeburg <mike@burg.me>
2025-06-09 19:00:12 -0700

    Bug fix for intake notes, and new year selector for pogs.

--- app/routes/person/pogs.js
+++ app/routes/person/pogs.js
@@ -1,18 +1,26 @@
 import ClubhouseRoute from 'clubhouse/routes/clubhouse-route';
+import requestYear from "clubhouse/utils/request-year";
 
 export default class PersonPogsRoute extends ClubhouseRoute {
-  model() {
-    return this.store.query('person-pog', {person_id: this.modelFor('person').id});
+  queryParams = {
+    year: {refreshModel: true}
+  };
+
+  model(params) {
+    const year = requestYear(params);
+    this.year = year;
+    return this.store.query('person-pog', {person_id: this.modelFor('person').id, year});
   }
 
   setupController(controller, model) {
-    controller.set('person', this.modelFor('person'));
-    controller.set('personPogs', model);
+    controller.person = this.modelFor('person');
+    controller.personPogs = model;
+    controller.year = this.year;
   }
 
   resetController(controller, isExiting) {
     if (isExiting) {
-      controller.set('year', null);
+      controller.year = null;
     }
   }
 }
