15e35b014ad4f20b66f4398a8efd591d6b746107
mikeburg <mike@burg.me>
2025-06-12 09:31:32 -0700

    Added csv export to early late checks report.

--- app/utils/duration-of-time.js
+++ app/utils/duration-of-time.js
@@ -7,7 +7,7 @@ export default function durationOfTime(rawMinutes) {
 
   if (days > 0) parts.push(`${days} day${days > 1 ? 's' : ''}`);
   if (hours > 0) parts.push(`${hours} hour${hours > 1 ? 's' : ''}`);
-  if (minutes > 0) parts.push(`${minutes} mins${minutes > 1 ? 's' : ''}`);
+  if (minutes > 0) parts.push(`${minutes} min${minutes > 1 ? 's' : ''}`);
 
   return parts.join(', ');
 }
