ca83e28c4ae62b9465f4b61ee86ad4f0a1d30818
mikeburg <frankenstein@burningman.org>
2025-07-11 19:48:19 -0700

    Fixed a crash when viewing prior schedule years. (#2330)

--- app/components/schedule-requirements.js
+++ app/components/schedule-requirements.js
@@ -1,10 +1,15 @@
 import Component from '@glimmer/component';
 import {requirementInfo} from 'clubhouse/constants/signup-requirements';
+import {cached} from '@glimmer/tracking';
 
 export default class ScheduleRequirementsComponent extends Component {
-  constructor() {
-    super(...arguments);
-    this.requirementsList = requirementInfo(this.args.person.status, this.args.requirements);
-    this.justTrainingBlocker = (!this.requirementsList.length && this.args.hasTrainingBlocker);
+  @cached
+  get requirementsList() {
+    return requirementInfo(this.args.person.status, this.args.requirements);
+  }
+
+  @cached
+  get justTrainingBlocker() {
+    return (!this.requirementsList.length && this.args.hasTrainingBlocker);
   }
 }
