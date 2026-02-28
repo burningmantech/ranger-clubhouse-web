9cdbf96d5ff3b6da0b683a2cfd6497ec984c23ac
mikeburg <frankenstein@burningman.org>
2026-02-27 20:44:22 -0800

    Team Roster Management modifications. (#2425)
    - Show when a position was granted.
    - Table layout improvements by Claude Code.

--- app/components/team-membership-row.js
+++ app/components/team-membership-row.js
@@ -2,11 +2,6 @@ import Component from '@glimmer/component';
 import {cached} from '@glimmer/tracking';
 
 export default class TeamMembershipRowComponent extends Component {
-  constructor() {
-    super(...arguments);
-
-  }
-
   @cached
   get allMembersPositions() {
     return this._buildPositions(this.args.allMembershipPositions);
@@ -22,13 +17,21 @@ export default class TeamMembershipRowComponent extends Component {
     return this._buildPositions(this.args.publicPositions);
   }
 
+  @cached
+  get allPositions() {
+    const tag = (positions) => positions.map((p, i) => ({...p, groupStart: i === 0}));
+    return [
+      ...tag(this.allMembersPositions),
+      ...tag(this.optionalPositions),
+      ...tag(this.publicPositions),
+    ];
+  }
+
   _buildPositions(positions) {
     const {person} = this.args;
-    return positions.map((p) => {
-      return {
-        title: p.title,
-        granted: person.positions.find((g) => p.id === g.id)
-      };
-    });
+    return positions.map((p) => ({
+      title: p.title,
+      granted: person.positions.find((g) => p.id === g.id),
+    }));
   }
 }
